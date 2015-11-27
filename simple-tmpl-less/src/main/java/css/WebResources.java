7c52f043901b8406b672c882d0594a23e5f5ff0d
astrapi69 <astrapi69@astrapi69-desktop>
2015-11-27 18:54:22 +0100

    renamed main less file and created a java class to load resources over the class loader.

---
+++ simple-tmpl-less/src/main/java/css/WebResources.java
@@ -0,0 +1,12 @@
+package css;
+
+import java.io.Serializable;
+
+/**
+ * The class {@link WebResources} serves as an entry point for get the web resources over the classpath.
+ */
+public final class WebResources implements Serializable {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 2486820976112828900L;
+}
