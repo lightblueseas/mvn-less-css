e8ee3865494d79b4c80cc32a5c2a160776cbe23b
astrapi69 <asterios.raptis@gmx.net>
2015-12-21 15:39:31 +0100

    moved to other package directories

---
+++ bootstrap-less/src/main/java/de/alpharogroup/web/WebResources.java
@@ -0,0 +1,12 @@
+package de.alpharogroup.web;
+
+import java.io.Serializable;
+
+/**
+ * The class {@link WebResources} serves as an entry point for get the web resources over the classpath.
+ */
+public final class WebResources implements Serializable {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 2486820976112828900L;
+}
